b8c48829807104276c812941914051a71000e33f
Dismes <yuhki.n@gmail.com>
2017-10-24 21:29:51 -0700

    gulp done

---
+++ assets/js/Final/script.js
@@ -0,0 +1,2 @@
+console.log("asdfasdfMAN");
+console.log("holyshit");
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,27 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var gulp = require('gulp');
+var uglify = require("gulp-uglify");
+var concat = require('gulp-concat');
 
-gulp.task('sass', function () {
-    return gulp.src('assets/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
-        .pipe(sass())
-        .pipe(gulp.dest('assets/css'))
+gulp.task('sass', function(){
+    return gulp.src('assets/scss/*.scss')
+      .pipe(sass()) // Converts Sass to CSS with gulp-sass
+      .pipe(gulp.dest('assets/css'));
+  });
+
+gulp.task('js', function(){
+    return gulp.src('../AnimeTesto/assets/js/*.js')
+    .pipe(uglify())
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('../AnimeTesto/assets/js/Final/'))
+})
+
+gulp.task('watch', function () {
+    gulp.watch('../AnimeTesto/assets/scss/*.scss', ['sass']);
+    gulp.watch('../AnimeTesto/assets/js/*.js', ['js']);
 })
 
 gulp.task('This', function () {
     console.log("Gulp.js");
-});
+});  
